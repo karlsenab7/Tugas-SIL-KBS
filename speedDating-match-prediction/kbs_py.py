0fdeb703e62f08f19f3638b8de83f1f88e5ab845
Joel <triwira.joel@gmail.com>
2022-11-16 23:41:51 +0700

    done

--- speedDating-match-prediction/kbs_py.py
+++ speedDating-match-prediction/kbs_py.py
@@ -8,44 +8,53 @@ import os
 import sys
 import pickle
 
+
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 
-df = pandas.read_csv('speedDating.csv', low_memory=False)
+df = pandas.read_csv('../speedDating.csv', low_memory=False)
 
 fields = df.field.str.lower().unique()
 field_choices = []
-for i in range (219):
+for i in range(219):
     field_choices.append(i+1)
 field_choices[218]
 
 df.replace(fields,
-		field_choices, inplace=True)
+           field_choices, inplace=True)
 
 df.replace(['male', 'female'],
-            [0,1], inplace=True)
+           [0, 1], inplace=True)
 
-col_names = df.columns.drop(['match'])
+col_names = df.columns.drop(['match', 'id', 'sports', 'tvsports', 'exercise', 'dining', 'museums', 'art', 'hiking',
+                             'gaming', 'clubbing', 'reading', 'tv', 'theater', 'movies', 'concerts', 'music', 'shopping', 'yoga', 'interests_correlate',
+                             'like', 'guess_prob_liked', 'met', 'expected_happy_with_sd_people', 'expected_num_interested_in_me', 'expected_num_matches', 'attractive', 'sincere', 'intelligence', 'funny', 'ambition', 'attractive_partner', 'sincere_partner',
+                             'intelligence_partner', 'funny_partner', 'ambition_partner', 'shared_interests_partner', 'pref_o_attractive', 'pref_o_sincere',
+                             'pref_o_intelligence', 'pref_o_funny', 'pref_o_ambitious', 'pref_o_shared_interests', 'attractive_o', 'sinsere_o', 'intelligence_o', 'funny_o', 'ambitous_o', 'decision', 'decision_o',
+                             'shared_interests_o', 'attractive_important', 'sincere_important', 'intellicence_important', 'funny_important', 'ambtition_important',
+                             'shared_interests_important'])
+
 
 X_train, X_test, y_train, y_test = train_test_split(
-		df[col_names], df['match'], test_size = 0.3, random_state = 100)
+    df[col_names], df['match'], test_size=0.3, random_state=100)
 
-clf_gini = DecisionTreeClassifier(criterion = "gini",
-                                     random_state = 100,
-                                     max_depth = 5,
-                                     min_samples_leaf = 5)
- 
-clf_gini.fit(X_train, y_train)
-y_pred_gini = clf_gini.predict(X_test)
- 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+r_predict = clf.predict(X_test)
+
+
+dt_clf_gini = DecisionTreeClassifier(criterion="gini",
+                                     random_state=100,
+                                     max_depth=5,
+                                     min_samples_leaf=5)
+
+dt_clf_gini.fit(X_train, y_train)
+y_pred_gini = dt_clf_gini.predict(X_test)
+
 acc_gini = accuracy_score(y_test, y_pred_gini) * 100
-print("Accuracy Score: ", acc,"%")
-print("F1 Score: ", f1,"%")
-print ("Accuracy scrore using Gini: ",
-             acc_gini,"%" )
-print()
 
-filePath = sys.path[0] + "speedDating-match-prediction\model.pkl"
-pickle.dump(clf_gini, open(filePath, 'wb'))
+
+filePath = sys.path[0] + "/model.pkl"
+pickle.dump(dt_clf_gini, open(filePath, 'wb'))
 print("saved to", filePath)
--- speedDating-match-prediction/script_flask.py
+++ speedDating-match-prediction/script_flask.py
@@ -5,23 +5,25 @@ import pickle
 app = Flask(__name__)
 
 # prediction function
-def ValuePredictor(to_predict_list):
-	to_predict = np.array(to_predict_list).reshape(1, 12)
-	loaded_model = pickle.load(open("model.pkl", "rb"))
-	result = loaded_model.predict(to_predict)
-	return result[0]
 
-@app.route('/Tugas-SIL-KBS/speedDating-match-prediction/templates/result', methods = ['POST', 'GET'])
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 8)
+    loaded_model = pickle.load(open("model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/Tugas-SIL-KBS/speedDating-match-prediction/templates/result', methods=['POST', 'GET'])
 def result():
-	if request.method == 'POST':
-		to_predict_list = request.form.to_dict()
-		to_predict_list = list(to_predict_list.values())
-		to_predict_list = list(map(int, to_predict_list))
-		result = ValuePredictor(to_predict_list)	
-		if int(result)== 1:
-			prediction ='Match'
-		else:
-			prediction ='Not Match'		
-		return render_template("index.html", prediction = prediction)
-	return render_template("result.html", prediction = prediction)
-        
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result) == 1:
+            prediction = 'Match'
+        else:
+            prediction = 'Not Match'
+        return render_template("result.html", prediction=prediction)
+    return render_template("index.html")
